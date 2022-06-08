3a174a8e4d5ac43c2c441796a405327e8db7737f
Alex Fernandez <104288796+alex-fernandez-89@users.noreply.github.com>
2022-06-07 21:26:28 -0300

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,24 @@
+/*footer*/
+function cartel() {
+    alert(
+        "Alex Fernandez - Contacto: Tel: 2616515447 - Email: alexfernandez.19899@gmail.com"
+    )
+}
+
+function inclusivo() {
+    document.body.style.backgroundColor="black";
+    document.getElementById("origen").style.color="gray";
+    document.getElementById("epi").style.color="white";
+    document.getElementById("cocinar").style.color="grey";
+    document.getElementById("ingredientes").style.color="white";
+    document.getElementById("masa").style.color="white";
+    document.getElementById("relleno").style.color="white";
+    document.getElementById("salsa").style.color="white";
+    document.getElementById("procedimiento").style.color="white";
+    document.getElementById("lamasa").style.color="white";
+    document.getElementById("elrelleno").style.color="white";
+    document.getElementById("formar").style.color="white";
+    document.getElementById("infoa").style.color="grey";
+    document.getElementById("parrafo").style.color="white";
+    
+}
